cf5eab1b89ad9e7857fd0c53b620c5dc873f0fb5
SarahSmallwood <133904630+SarahSmallwood@users.noreply.github.com>
2023-08-03 22:55:26 -0400

    Api to axios

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 function Projects() {
@@ -6,18 +7,18 @@ function Projects() {
     const [repo, setRepo] = useState([])
 
     useEffect(() => {
-        const fetchRepo = async () => {
-            const res = await fetch(`http://api.github.com/users/${user}/repos`)
+        const getRepo = async () => {
+            let res = await axios.get(`http://api.github.com/users/${user}/repos`);
             const data = await res.json()
             setRepo(data.user.repo)
         }
-        fetchRepo()
+        getRepo()
     },[])
   return (
     <div>
     <h1 className='projectTitle'> Sarah's Portfolio Projects </h1>
     <div>
-        {data.map.repo}
+        {user.map.repo}
     </div>
     </div> 
     
