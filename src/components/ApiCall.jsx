4adca528889b8d2e93726f76729864c912456bb8
SarahSmallwood <133904630+SarahSmallwood@users.noreply.github.com>
2023-08-06 22:45:34 -0400

    layout change

--- src/components/ApiCall.jsx
+++ src/components/ApiCall.jsx
@@ -47,20 +47,46 @@ function ApiCall() {
 
                 
                 <div className='url'>
-                <a className='apiLink' href={repo[9].html_url}> {repo[9].html_url} </a>
-                <a className='apiLink' href={repo[11].html_url}> {repo[11].html_url} </a>
-                <a className='apiLink' href={repo[14].html_url}> {repo[14].html_url} </a>
-                <a className='apiLink' href={repo[15].html_url}> {repo[15].html_url} </a>
-                <a className='apiLink' href={repo[16].html_url}> {repo[16].html_url} </a>
-                <a className='apiLink' href={repo[20].html_url}> {repo[20].html_url} </a>
-                </div>
-                <div className='img'>
+                    <div className='game'>
+
                     <img src="https://media.discordapp.net/attachments/1135935108650635327/1137931317397430312/TriviaGame.png?width=1828&height=699" about='alt' className='trivia'/>
+
+                    <a className='apiLink' href={repo[9].html_url}> {repo[9].html_url} </a>
+                        
+                    </div>
+
+                    <div className='perScholas'>
                     <img src="https://media.discordapp.net/attachments/1135935108650635327/1137933878577868871/per.png?width=1559&height=840" about='alt' className='trivia'/>
+                    <a className='apiLink' href={repo[11].html_url}> {repo[11].html_url} </a>
+                    </div>
+
+                    <div className='resume'>
                     <img src="https://media.discordapp.net/attachments/1135935108650635327/1137933879957803140/perscholas.png?width=1157&height=840" about='alt' className='trivia'/>
+                    <a className='apiLink' href={repo[14].html_url}> {repo[14].html_url} </a>
+                    </div>
+
+
+                    <div className='fourth'>
                     <img src="https://media.discordapp.net/attachments/1135935108650635327/1137933878938587147/space.png?width=1828&height=770" about='alt' className='trivia'/>
+                    <a className='apiLink' href={repo[15].html_url}> {repo[15].html_url} </a>
+
+                    </div>
+
+                    <div className='fifth'>
                     <img src="https://media.discordapp.net/attachments/1135935108650635327/1137933879488032849/starbucks.png?width=1535&height=840" about='alt' className='trivia'/>
+                    <a className='apiLink' href={repo[16].html_url}> {repo[16].html_url} </a>
+                    </div>
+
+                    <div className='sixth'>
                     <img src="https://media.discordapp.net/attachments/1135935108650635327/1137933878267494491/wholefoods.png?width=1551&height=838" about='alt' className='trivia'/>
+                    <a className='apiLink' href={repo[20].html_url}> {repo[20].html_url} </a>
+                    </div>
+                
+                
+                
+                
+                
+                
                 </div>
             </div>
         );
